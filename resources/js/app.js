0aa6576f97493b66f0fe5adebb66fbf80f98712d
Steve McDougall <juststevemcd@gmail.com>
2025-10-06 10:32:16 +0100

    chore: getting set up

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import Alpine from "alpinejs";
+import intersect from "@alpinejs/intersect";
+import axios from "axios";
+
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Alpine = Alpine
+Alpine.plugin(intersect);
+Alpine.start()
